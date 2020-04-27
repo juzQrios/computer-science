6f6cc59999ab65a522bc8ab6170aeeb701dd08d7
Darshan <dj30c.1@gmail.com>
2020-04-27 23:43:23 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Computer Science</h1>
+<div>
+  Concrete purpose of this repository is still vague. Being used to store algorithms for now.
+</div>
+`;
