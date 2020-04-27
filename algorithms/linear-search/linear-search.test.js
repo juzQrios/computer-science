ada5c7b6adeab5ad16a6ce90dc7ca48fc2f94403
juzQrios <dj30c.1@gmail.com>
2020-04-28 01:21:18 +0530

    Add Linear Search tests

--- algorithms/linear-search/linear-search.test.js
+++ algorithms/linear-search/linear-search.test.js
@@ -5,3 +5,7 @@ const array = [2, 10, 7, 5, 24, 11, 6];
 test("Successful Linear Search", () => {
   expect(linearSearch(array, 7)).toBe(2);
 });
+
+test("Unsuccessful Linear Search", () => {
+  expect(linearSearch(array, 1)).toBe(-1);
+});
