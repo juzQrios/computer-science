77eee424a3910157801859a035665745c1b69966
juzQrios <dj30c.1@gmail.com>
2020-04-28 01:18:23 +0530

    Remove unwanted files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "./styles.css";
-
-document.getElementById("app").innerHTML = `
-<h1>Computer Science</h1>
-<div>
-  Concrete purpose of this repository is still vague. Being used to store algorithms for now.
-</div>
-`;
