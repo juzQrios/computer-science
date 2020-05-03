46f4846fb8457b079641d7afeb3257104e46c879
juzQrios <dj30c.1@gmail.com>
2020-05-03 15:28:09 +0530

    Add recursive binary search

--- algorithms/binary-search/binary-search.test.js
+++ algorithms/binary-search/binary-search.test.js
@@ -1,17 +1,54 @@
-import binarySearch1 from './1';
+import iterativeBinarySearch from './iterative';
+import recursiveBinarySearch from './recursive';
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
+const single = [10];
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
   41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
-test('should return target index', () => {
-  expect(binarySearch1(array, 2)).toBe(1);
+
+//--- Iterative ---//
+describe('Iterative binary search', () => {
+  test('should return target index if found', () => {
+    expect(iterativeBinarySearch(array, 2)).toBe(1);
+  });
+
+  test('should return target index if found', () => {
+    expect(iterativeBinarySearch(primes, 73)).toBe(20);
+  });
+
+  test('should return -1 if target not found', () => {
+    expect(iterativeBinarySearch(primes, 72)).toBe(-1);
+  });
+
+  test('should return -1 if target not found', () => {
+    expect(iterativeBinarySearch(single, 72)).toBe(-1);
+  });
+
+  test('should return target index if found', () => {
+    expect(iterativeBinarySearch(single, 10)).toBe(0);
+  });
 });
 
-test('should return target index', () => {
-  expect(binarySearch1(primes, 73)).toBe(20);
-});
+//--- Recursive ---//
+describe('Recursive binary search', () => {
+  test('should return target index if found', () => {
+    expect(recursiveBinarySearch(array, 2)).toBe(1);
+  });
 
-test('should return -1', () => {
-  expect(binarySearch1(primes, 72)).toBe(-1);
+  test('should return target index if found', () => {
+    expect(recursiveBinarySearch(primes, 73)).toBe(20);
+  });
+
+  test('should return -1 if target not found', () => {
+    expect(recursiveBinarySearch(primes, 72)).toBe(-1);
+  });
+
+  test('should return -1 if target not found', () => {
+    expect(recursiveBinarySearch(single, 72)).toBe(-1);
+  });
+
+  test('should return target index if found', () => {
+    expect(recursiveBinarySearch(single, 10)).toBe(0);
+  });
 });
---
+++ algorithms/binary-search/recursive.js
@@ -0,0 +1,15 @@
+const recursiveBinarySearch = (array, target, low = 0, high = array.length - 1) => {
+  if(low > high) {
+    return -1;
+  } 
+  const mid = Math.round((low + high)/2);
+  if(array[mid] === target) {
+    return mid;
+  } else if (array[mid] > target) {
+    return recursiveBinarySearch(array, target, low, mid - 1);
+  } else {
+    return recursiveBinarySearch(array, target, mid + 1, high);
+  }
+}
+
+export default recursiveBinarySearch;
