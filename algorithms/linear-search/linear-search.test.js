32751789ac518b34d3fd3cdae35c48a2da16ae88
juzQrios <dj30c.1@gmail.com>
2020-05-03 15:28:50 +0530

    Update tests

--- algorithms/linear-search/linear-search.test.js
+++ algorithms/linear-search/linear-search.test.js
@@ -2,10 +2,12 @@ import linearSearch from "./linear-search";
 
 const array = [2, 10, 7, 5, 24, 11, 6];
 
-test("Successful Linear Search", () => {
-  expect(linearSearch(array, 7)).toBe(2);
-});
+describe('Linear search', () => {
+  test("should return target index if found", () => {
+    expect(linearSearch(array, 7)).toBe(2);
+  });
 
-test("Unsuccessful Linear Search", () => {
-  expect(linearSearch(array, 1)).toBe(-1);
+  test("should return -1 if target not found", () => {
+    expect(linearSearch(array, 1)).toBe(-1);
+  });
 });
--- algorithms/reverse-string/reverse-string.test.js
+++ algorithms/reverse-string/reverse-string.test.js
@@ -2,23 +2,24 @@ import reverseString1 from './1';
 import reverseString2 from './2';
 import reverseString3 from './3';
 
+describe('Reverse string functions', () => {
+  // Built-in functions
+  test('should reverse passed string', () => {
+    expect(reverseString1('Hello')).toBe('olleH');
+  });
 
-// Built-in functions
-test('should reverse passed string', () => {
-  expect(reverseString1('Hello')).toBe('olleH');
-});
+  // Decrementing loop concatination
+  test('should reverse passed string', () => {
+    expect(reverseString2('Hello')).toBe('olleH');
+  });
 
-// Decrementing loop concatination
-test('should reverse passed string', () => {
-  expect(reverseString2('Hello')).toBe('olleH');
-});
+  // Array mirror swap - odd length
+  test('should reverse passed string', () => {
+    expect(reverseString3('Hello')).toBe('olleH');
+  });
 
-// Array mirror swap - odd length
-test('should reverse passed string', () => {
-  expect(reverseString2('Hello')).toBe('olleH');
-});
-
-// Array mirror swap - even length
-test('should reverse passed string', () => {
-  expect(reverseString2('AllMight')).toBe('thgiMllA');
+  // Array mirror swap - even length
+  test('should reverse passed string', () => {
+    expect(reverseString3('AllMight')).toBe('thgiMllA');
+  });
 });
