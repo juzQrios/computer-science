0b6608f0705c2ef6b80a03a73a9c0419e750ef80
juzQrios <dj30c.1@gmail.com>
2020-05-12 19:27:25 +0530

    Add insertion sort

---
+++ algorithms/insertion-sort/insertion-sort.test.js
@@ -0,0 +1,17 @@
+import insertionSort from './insertion-sort';
+
+const array1 = [25, 15, 5, 2, 10, 9];
+const array2 = [25, 15, 5, 0, 10, 9];
+const array3 = [25, -15, 5, 0, -25, 9];
+
+describe('Insertion sort', () => {
+  test('should sort passed array', () => {
+    expect(insertionSort(array1)).toStrictEqual([2, 5, 9, 10, 15, 25]);
+  });
+  test('should sort passed array', () => {
+    expect(insertionSort(array2)).toStrictEqual([0, 5, 9, 10, 15,25]);
+  });
+  test('should sort passed array', () => {
+    expect(insertionSort(array3)).toStrictEqual([-25, -15, 0, 5, 9, 25]);
+  });
+});
